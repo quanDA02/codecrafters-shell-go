6cdec5709e16865e6bb02ae9b58f053e721b8e4e
quanDA02 <quanda2k3@gmail.com>
2026-08-12 14:13:45 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -10,4 +11,8 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+	command := os.Args[1]
+	if command != "" {
+		fmt.Println(command, ": command not found")
+	}
 }
