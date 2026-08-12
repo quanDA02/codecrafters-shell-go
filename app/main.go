e164ccd790854268de651281b9157cd85df105b3
quanDA02 <quanda2k3@gmail.com>
2026-08-12 22:37:06 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,10 +22,9 @@ func main() {
 			break
 		} else if strings.HasPrefix(command, "echo ") {
 			fmt.Println(command[5:])
+		} else {
+			fmt.Println(command + ": command not found")
 		}
-		// else {
-		// 	fmt.Println(command + ": command not found")
-		// }
 	}
 
 }
