4e434ec2ba53574c4bc5a3972320bb378fadb57e
quanDA02 <quanda2k3@gmail.com>
2026-08-13 22:52:03 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,13 +12,9 @@ import (
 	"github.com/google/shlex"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-
 		fmt.Print("$ ")
 		command, _ := reader.ReadString('\n')
 		command = strings.TrimSpace(command)
@@ -48,6 +44,7 @@ func echo(s []string) {
 	text := strings.Join(s, " ")
 	for i, cmd := range s {
 		if cmd == ">" || cmd == "1>" {
+			fmt.Println(cmd)
 			err := os.WriteFile(s[i], []byte(text), 0666)
 			if err != nil {
 				log.Fatal(err)
