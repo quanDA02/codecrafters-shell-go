19d490241f16670ba18c472e36af1864a7bfba0a
quanDA02 <quanda2k3@gmail.com>
2026-08-14 13:19:53 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,34 +13,6 @@ import (
 	"github.com/google/shlex"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("$ ")
-		command, _ := reader.ReadString('\n')
-		command = strings.TrimSpace(command)
-
-		//literally the strings split but on crack
-		tokens, _ := shlex.Split(command)
-		//check if command : exit
-		if command == "exit" {
-			break
-		} else if strings.HasPrefix(command, "echo ") {
-			echo(command)
-		} else if strings.HasPrefix(command, "type ") {
-			typeCommand(command[5:])
-		} else {
-			path := findPath(tokens[0])
-			if path != "" {
-				execute(tokens[0], tokens[1:])
-			} else {
-				fmt.Println(command + ": command not found")
-			}
-
-		}
-	}
-}
-
 func echo(s string) {
 	tokens := strings.Split(s, " ")
 
@@ -113,13 +85,42 @@ func findPath(file string) string {
 	return path
 }
 
-func execute(name string, args []string) {
-	cmd := exec.Command(name, args...)
+func execute(name string) error {
+	cmd := exec.Command(name)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
+	return cmd.Run()
 }
 
-func redirect() {
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		command, _ := reader.ReadString('\n')
+		command = strings.TrimSpace(command)
 
+		if err := execute(command); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		//literally the strings split but on crack
+		tokens, _ := shlex.Split(command)
+
+		//check if command : exit
+		if command == "exit" {
+			break
+		} else if strings.HasPrefix(command, "echo ") {
+			echo(command)
+		} else if strings.HasPrefix(command, "type ") {
+			typeCommand(command[5:])
+		} else {
+			path := findPath(tokens[0])
+			if path != "" {
+				execute(tokens[0], tokens[1:])
+			} else {
+				fmt.Println(command + ": command not found")
+			}
+
+		}
+	}
 }
