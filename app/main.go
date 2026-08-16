5656ad62827107bcc2b353b550c745dc5382bb52
quanDA02 <quanda2k3@gmail.com>
2026-08-16 22:19:53 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -28,6 +28,7 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 	if len(newline) > 1 {
 		slices.SortFunc(newline, slices.Compare)
 		b := buildinPrefix(newline)
+		fmt.Print(b)
 		if len(b) > 0 {
 			return newline, length
 		}
