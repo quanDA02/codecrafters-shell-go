44f8fe905683c3cb713e1014749d51c6178e4365
quanDA02 <quanda2k3@gmail.com>
2026-08-17 13:48:21 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,7 +21,7 @@ type completerBell struct {
 func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 	newline, length := c.completer.Do(line, pos)
 	//bell sound if autocomplete fail
-	fmt.Print(newline)
+	fmt.Print(len(newline))
 	if len(newline) == 0 {
 		fmt.Print("\x07")
 		return nil, 0
