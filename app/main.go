a342ddcda28dd17cb8375727886fbdb197964ab0
quanDA02 <quanda2k3@gmail.com>
2026-08-17 22:59:04 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -103,7 +103,7 @@ func executableCompletion(prefixes string) []string {
 	} else {
 		files, _ := os.ReadDir("./")
 		if strings.Contains(prefixes, "/") {
-			fmt.Print(last)
+			// fmt.Print(last)
 			files, _ = os.ReadDir(prefixes)
 		}
 
