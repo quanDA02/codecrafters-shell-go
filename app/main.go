c0ac8c25bfec4ba2e2c06e2939cbcad225f85269
quanDA02 <quanda2k3@gmail.com>
2026-08-17 14:33:25 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -24,7 +24,8 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 	for i, r := range line {
 		if r == ' ' {
 			lline = line[i:]
-			pos = len(line)
+			fmt.Print("len line: ", lline)
+			pos = len(lline)
 		}
 	}
 	newline, length := c.completer.Do(lline, pos)
