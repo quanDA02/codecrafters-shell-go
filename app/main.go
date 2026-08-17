470bdbb488acf336525612ffa97194e4450b11e5
quanDA02 <quanda2k3@gmail.com>
2026-08-17 14:49:08 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -72,7 +72,7 @@ func commonPrefix(line [][]rune) []rune {
 
 func executableCompletion(prefixes string) []string {
 
-	prefix, _ := shlex.Split(prefixes)
+	prefix := strings.Split(prefixes, " ")
 	first, last := prefix[0], prefix[len(prefix)-1]
 	suggestions := make([]string, 0)
 	if first == last {
