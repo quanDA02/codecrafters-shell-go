c6ef1cd5bd4551ce4e46a0f67d48d1c3411dc0cb
quanDA02 <quanda2k3@gmail.com>
2026-08-17 18:34:31 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -122,7 +122,7 @@ func executableCompletion(prefixes string) []string {
 		}
 	}
 
-	fmt.Println("suggestion :", suggestions)
+	// fmt.Println("suggestion :", suggestions)
 
 	return suggestions
 }
