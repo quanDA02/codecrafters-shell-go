2e1e6358c7a5e5b065e659857de23306392c6e1d
quanDA02 <quanda2k3@gmail.com>
2026-08-18 17:41:08 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -56,12 +56,11 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 			c.tabCount++
 		} else {
 			c.tabCount = 0
-			fmt.Println()
+			fmt.Println("\n")
 			// print sorted suggestions
 			for _, suggestion := range newline {
 				fmt.Print(string(line), string(suggestion))
 			}
-			fmt.Println()
 			fmt.Print("\n$ " + string(line))
 		}
 		return nil, 0
