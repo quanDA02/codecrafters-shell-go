56711fe17f33224a76a9176312628572d394ead1
quanDA02 <quanda2k3@gmail.com>
2026-08-19 23:15:25 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -35,8 +35,8 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 	newline, length := c.completer.Do(line, pos)
 
 	//these print line for debug purpose only
-	fmt.Println("line:", string(line))
-	fmt.Println("new:", len(newline))
+	// fmt.Println("line:", string(line))
+	// fmt.Println("new:", len(newline))
 
 	//bell sound if autocomplete fail
 	if len(newline) == 0 {
