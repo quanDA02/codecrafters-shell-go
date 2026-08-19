659e2b6bba871cbfb5e0dbf0cad4d4d86ce3f06c
quanDA02 <quanda2k3@gmail.com>
2026-08-19 15:12:53 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -101,6 +101,7 @@ func executableCompletion(prefixes string) []string {
 		files, _ := os.ReadDir("./")
 		if strings.HasSuffix(prefixes, "_") {
 			fmt.Print("waduh")
+			prefixes = strings.TrimSpace(prefixes)
 			files, _ = os.ReadDir(prefixes)
 		}
 		for _, file := range files {
