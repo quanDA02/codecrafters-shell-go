d074a2ffc48d2bfb374797d25f09a0dc3aa7a44f
quanDA02 <quanda2k3@gmail.com>
2026-08-19 14:41:35 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -103,7 +103,7 @@ func executableCompletion(prefixes string) []string {
 		dirs := filepath.SplitList(path)
 		for _, dir := range dirs {
 			files, _ := os.ReadDir(dir)
-			// files, _ = os.ReadDir("./")
+			files, _ = os.ReadDir("./")
 			for _, file := range files {
 				suggestions = append(suggestions, file.Name())
 			}
