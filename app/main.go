cccc63ff52f1663e4b51d67d205612f344db73d2
quanDA02 <quanda2k3@gmail.com>
2026-08-19 14:44:11 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -110,7 +110,6 @@ func executableCompletion(prefixes string) []string {
 		// fmt.Println("suggestion :", suggestions)
 		// fmt.Print("sss")
 	} else {
-		println("sss")
 		for _, file := range files {
 			name := file.Name()
 			//check if the last character is a "/"(slash)
