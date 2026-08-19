ae8437dd4d567db616e2412e4d3d5232009a5f62
quanDA02 <quanda2k3@gmail.com>
2026-08-19 15:04:56 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -159,7 +159,7 @@ func executableCompletion(prefixes string) []string {
 						name = file.Name() + "/"
 						suggestions = append(suggestions, name)
 					} else {
-						suggestions = append(suggestions, file.Name()+"tst")
+						suggestions = append(suggestions, file.Name())
 					}
 				}
 			}
