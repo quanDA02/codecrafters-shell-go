ac4310f4eb325b1dc9c9983b0558dd0ff31c35e1
quanDA02 <quanda2k3@gmail.com>
2026-08-19 15:02:39 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -152,7 +152,7 @@ func executableCompletion(prefixes string) []string {
 				name := file.Name()
 				//check if the last character is a "/"(slash)
 				if strings.HasPrefix(name, "/") {
-					suggestions = append(suggestions, file.Name())
+					suggestions = append(suggestions, prefixes+file.Name())
 				} else {
 					// check if file name is a directory
 					if file.IsDir() {
