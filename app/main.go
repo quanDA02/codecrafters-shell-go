77a6b7c9fe3a511a5c3ea1f4407d4797ce5db776
quanDA02 <quanda2k3@gmail.com>
2026-08-20 23:02:45 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -219,7 +219,7 @@ func redirect(args []string, stdout, stderr *os.File) ([]string, *os.File, *os.F
 }
 
 func complete(args []string) {
-	fmt.Printf("comeplete:")
+	fmt.Printf("\ncomeplete:")
 	// fmt.Printf("complete: %s: no completion specification", args)
 }
 
