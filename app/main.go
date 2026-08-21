c89d6fc2a80a8d4d95b12aaa3ddde84316ac9d97
quanDA02 <quanda2k3@gmail.com>
2026-08-21 17:49:34 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,7 +21,7 @@ type completerBell struct {
 // modified autocomplete Do and add bell sound
 func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 
-	fmt.Println(line[0])
+	fmt.Println(string(line[0]))
 
 	lline := line
 	arr, _ := shlex.Split(string(line))
