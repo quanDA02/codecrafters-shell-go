26555c8df4696c5965ac2068b2e777b0547768a5
quanDA02 <quanda2k3@gmail.com>
2026-08-21 21:59:17 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -64,7 +64,7 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 			c.tabCount = 0
 			fmt.Println()
 			// print sorted suggestions
-
+			fmt.Println(len(newline))
 			prefix := strings.TrimSpace(string(line))
 
 			for _, suggestion := range newline {
