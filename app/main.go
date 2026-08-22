337566d880ad5b3abd39b1c2fca10be36b6ddf21
quanDA02 <quanda2k3@gmail.com>
2026-08-22 22:40:03 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -302,19 +302,22 @@ func complete(args []string) {
 		return
 	}
 	flag := args[0]
+	key := args[1]
 	switch flag {
 	case "-p":
-		key := args[1]
+
 		path, exist := completeMap[key]
 		if exist {
 			fmt.Printf("complete -C '%s' %s\n", path, key)
 		} else {
-			fmt.Printf("complete: %s: no completion specification\n", args[1])
+			fmt.Printf("complete: %s: no completion specification\n", key)
 		}
 	case "-C":
 		if len(args) > 2 {
 			completeMap[args[2]] = args[1]
 		}
+	case "-r":
+		delete(completeMap, key)
 	}
 }
 
