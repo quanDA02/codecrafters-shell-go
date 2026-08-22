6b61e05d36416c753e2f92cfcd3229636db9b129
quanDA02 <quanda2k3@gmail.com>
2026-08-22 21:53:18 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -47,7 +47,7 @@ func (c *completerBell) Do(line []rune, pos int) ([][]rune, int) {
 	}
 	//these print line for debug purpose only
 	// fmt.Println("line:", string(line))
-	// fmt.Println("new:", len(newline))
+	fmt.Println("new:", len(newline))
 	//bell sound if autocomplete fail
 	if len(newline) < 1 {
 		fmt.Print("\x07")
