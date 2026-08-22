ed9f7cd98c806612cd7af2924fd9b8d65657d398
quanDA02 <quanda2k3@gmail.com>
2026-08-22 22:20:08 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -143,6 +143,7 @@ func executableCompletion(prefixes string) []string {
 			prev = strings.TrimSpace(command)
 			if command != prev {
 				suggestion = command + " " + prev + " " + suggestion
+				println(suggestion)
 			} else {
 				suggestion = command + " " + suggestion
 			}
