078ac4e11dbf18cc9e72705aeabe49811bd92508
quanDA02 <quanda2k3@gmail.com>
2026-08-22 18:00:38 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -118,6 +118,7 @@ func executableCompletion(prefixes string) []string {
 			}
 			output := strings.TrimSpace(string(out))
 			suggestions = append(suggestions, command+" "+previousWord+" "+output)
+			fmt.Println(suggestions)
 		}
 
 		return suggestions
