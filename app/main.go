1a6df7d2c39381c0a928825e9596e295a1705a37
quanDA02 <quanda2k3@gmail.com>
2026-08-22 22:18:23 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -139,13 +139,10 @@ func executableCompletion(prefixes string) []string {
 
 		// suggestion := output
 		for _, suggestion := range output {
-			suggestion = command + " " + suggestion
 			if command != prev {
-				// suggestion = command + " " + prev + " " + suggestion
-				fmt.Println("\n", command, " 1")
-				fmt.Println("\n", prev, " 2")
-				fmt.Println("\n", suggestion, " 3")
-				// fmt.Println("\n", suggestion, " dsdsd")
+				suggestion = command + " " + prev + " " + suggestion
+			} else {
+				suggestion = command + " " + suggestion
 			}
 			suggestions = append(suggestions, suggestion)
 		}
