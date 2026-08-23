acd3968eb9a0b72ba499b45e9569afa85f39dd86
quanDA02 <quanda2k3@gmail.com>
2026-08-23 17:56:59 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -342,7 +342,7 @@ func jobs() {
 
 		if job.status == "Done" {
 			names := strings.Split(job.name, " ")
-			name := strings.Join(names[0:len(names)-2], " ")
+			name := strings.Join(names[0:len(names)-1], " ")
 			fmt.Println(name)
 		}
 		fmt.Printf("[%d]%s  %-24s%s\n", job.id, mark, job.status, job.name)
