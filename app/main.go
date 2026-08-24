c8507f07ddc1d874ff6bcba598b35a8dc5f88c18
quanDA02 <quanda2k3@gmail.com>
2026-08-24 17:31:01 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -400,8 +400,8 @@ func execute(name string) {
 		case "type":
 			typeCommand(name[5:])
 			return
-		case "echo":
-			echo(strings.Join(args[1:], " "), stdout)
+		// case "echo":
+		// 	echo(strings.Join(args[1:], " "), stdout)
 		case "complete":
 			complete(args[1:])
 			return
