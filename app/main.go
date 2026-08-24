6d4d0f2f7d87751fb473e44e5c944a4f4754069f
quanDA02 <quanda2k3@gmail.com>
2026-08-24 13:29:56 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -296,7 +296,7 @@ func redirect(args []string, stdin, stdout, stderr *os.File) ([]string, *os.File
 		}
 		stdout = w
 		stdin = r
-		args = args[len(args)-1:]
+		args = append(args[:len(args)-2], args[len(args)-1])
 		return args, stdin, stdout, stderr
 	default:
 		return args, stdin, stdout, stderr
