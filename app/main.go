95a1c1790dd9a81f0a6a8b43c35d1ce07454afa2
quanDA02 <quanda2k3@gmail.com>
2026-08-24 14:44:01 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -471,11 +471,9 @@ func pipelineExecute(name string) {
 			cmd.Stdout = w
 			prev = r
 			cmd.Start()
-			w.Close()
 		} else {
 			cmd.Stdout = os.Stdout
 			cmd.Start()
-			prev.Close()
 		}
 		cmd.Wait()
 	}
