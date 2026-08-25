52eb00c9534f2e6b2e948533c5c0494ebd95146c
quanDA02 <quanda2k3@gmail.com>
2026-08-25 23:15:37 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -554,6 +554,7 @@ func declare(command string) {
 	switch args[0] {
 	case "-p":
 		key := args[1]
+		fmt.Println(key)
 		if value, exist := variables[key]; exist {
 			fmt.Printf("declare -- %s=%s", key, value)
 		} else {
