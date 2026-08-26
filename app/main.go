90f74a0dca37d1a44c201f291fdf048e68bfc28f
quanDA02 <quanda2k3@gmail.com>
2026-08-26 19:08:15 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -499,6 +499,9 @@ func pwd() {
 }
 
 func cd(path string) {
+	if path == "~" {
+		path = os.Getenv("HOME")
+	}
 	err := os.Chdir(path)
 	if err != nil {
 		fmt.Printf("cd: %s: No such file or directory\n", path)
